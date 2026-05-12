53422747d0b8bfeceebf292d067f1b316aba5405
mfwolffe <wolffemf@dukes.jmu.edu>
2026-05-12 13:05:47 -0400

    pr reopen: tests for state patch

---
+++ pkg/cmd/pr/reopen/reopen_test.go
@@ -0,0 +1,41 @@
+// SPDX-License-Identifier: AGPL-3.0-or-later
+
+package reopen
+
+import (
+	"context"
+	"encoding/json"
+	"io"
+	"net/http"
+	"testing"
+
+	"github.com/tenseleyFlow/shithub-cli/internal/cmdutil/cmdutiltest"
+	"github.com/tenseleyFlow/shithub-cli/internal/pulls"
+)
+
+func TestReopenPatchesStateOpen(t *testing.T) {
+	tf := cmdutiltest.New(t)
+	var body json.RawMessage
+	tf.Server.Handle(http.MethodPatch, "/api/v1/repos/o/r/pulls/1", func(w http.ResponseWriter, r *http.Request) {
+		b, _ := io.ReadAll(r.Body)
+		body = b
+		w.Header().Set("Content-Type", "application/json")
+		_ = json.NewEncoder(w).Encode(pulls.PR{Number: 1, State: "open"})
+	})
+
+	opts := &options{
+		IO:          tf.IOStreams,
+		HTTPClient:  tf.Factory.HTTPClient,
+		DefaultHost: tf.Factory.DefaultHost,
+		Arg:         "1",
+		Repo:        "o/r",
+	}
+	if err := Run(context.Background(), opts); err != nil {
+		t.Fatalf("Run: %v", err)
+	}
+	var p map[string]any
+	_ = json.Unmarshal(body, &p)
+	if p["state"] != "open" {
+		t.Errorf("state: %v", p)
+	}
+}
