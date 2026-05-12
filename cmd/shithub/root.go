5c6ee7c2bac92f4eaa86ee160bebddfd1b2f2979
mfwolffe <wolffemf@dukes.jmu.edu>
2026-05-12 13:06:10 -0400

    cmd/shithub: register pr subtree on root

--- cmd/shithub/root.go
+++ cmd/shithub/root.go
@@ -20,6 +20,7 @@ import (
 	"github.com/tenseleyFlow/shithub-cli/pkg/cmd/completion"
 	configcmd "github.com/tenseleyFlow/shithub-cli/pkg/cmd/config"
 	issuecmd "github.com/tenseleyFlow/shithub-cli/pkg/cmd/issue"
+	prcmd "github.com/tenseleyFlow/shithub-cli/pkg/cmd/pr"
 	repocmd "github.com/tenseleyFlow/shithub-cli/pkg/cmd/repo"
 )
 
@@ -138,4 +139,5 @@ func init() {
 	rootCmd.AddCommand(completion.NewCmd(f))
 	rootCmd.AddCommand(repocmd.NewCmd(f))
 	rootCmd.AddCommand(issuecmd.NewCmd(f))
+	rootCmd.AddCommand(prcmd.NewCmd(f))
 }
